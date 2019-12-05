89441693d48ccf21682a39ab5bbbea897c5704fb
Michael Zapata <mikeyfriends@hotmail.com>
2019-12-05 10:51:24 +0000

    multiple pointers unique values

--- Common PS patterns/multiplePointers.js
+++ Common PS patterns/multiplePointers.js
@@ -2,9 +2,6 @@
 // The function should find the first pair where the sum is 0. 
 // Return an array that includes both values that sum to zero or undefined if a pair does not exist
 
-// sumZero([-3,-2,-1,0,1,2,3]) // [-3,3] 
-// sumZero([-2,0,1,3]) // undefined
-// sumZero([1,2,3]) // undefined
 
 // Time Complexity - O(N^2) -> 2 nested loops
 // Space Complexity - O(1)
@@ -22,24 +19,47 @@ function sumZero(arr){
 //BETTER: Using muliple pointers -> One on the LEFT-MOST side and another on the RIGHT-MOST side, they will move to the middle
 //Inputs: linear structure,targetSum
 //outputs:  [X:Y]
-//Requirements: We need the list to be SORTED.
+//Requirements: We need the list to be SORTED. 
+// -> SO, if we want the sum to be Smaller, move Right pointer to left. 
+// -> ELSE, if we want the sum to be Bigger, move the Left pointer to right.
 //Assumptions: There is at least one pair adding up to the targetSum
-
-function sumZero(arr, targetSum = 0){
+function sumZeroPointers(arr, targetSum = 0){
     //SORT THE ARRAY:
-    arr.sort();//will sort the original array in ASC
+    arr.sort(function(a, b){return a - b});
+    // When the sort() function compares two values, it sends the values to the compare function, and sorts the values according to the returned (negative, zero, positive) value.
+    // If the result is negative a is sorted before b.
+    // If the result is positive b is sorted before a.
+    // If the result is 0 no changes are done with the sort order of the two values.
+
     //Create 2 pointers
     //Left-most pointer
-    const left = 0;
+    let left = 0;
     //Right-most pointer
-    const right = arr.length -1;
+    let right = arr.length -1;
 
-    while( left < right ){
-
+    //This should be true(as long as both pointers are not pointing to the same element) as the LIST is ordered in ASC order
+    while( left < right ){//We need < instead of <= so we stop when both pointers point to the same element.
+        let sum = arr[left] + arr[right];
+        if(sum === targetSum){
+            return [arr[left], arr[right]];
+        } else if(sum > targetSum){
+            //Sum is too big, so make it smaller
+            //by moving the Right pointer to left
+            right--;
+        } else {//sum < targetSum
+            //Sum is too small, so make it bigger
+            //by moving the Left pointer to right
+            left++;
+        }
     }
-
-
+    return [];
 }
+//Time Complexity - O(N)
+//Space Complexity - O(1)
+
+console.log(sumZeroPointers([-3,-2,-1,0,1,2,3])) // [-3,3] 
+console.log(sumZeroPointers([-2,0,1,3])) // undefined
+console.log(sumZeroPointers([1,2,3])) // undefined
 
 
 
---
+++ Common PS patterns/multiplePointersUniqueValues.js
@@ -0,0 +1,51 @@
+// Implement a function called countUniqueValues, which accepts a sorted array, and counts the unique values in the array. 
+
+// Input: Sorted Array
+// Output: Int(how many unique values)
+// Output Determined from: Counting the UNIQUE values in the array.
+// Assumptions: There can be negative numbers in the array
+// Requirements: Array will always be sorted.
+
+//Test cases:
+countUniqueValues([1,1,1,1,1,2]) // 2
+countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]) // 7
+countUniqueValues([]) // 0
+countUniqueValues([-2,-1,-1,0,1]) // 4
+
+//Solution using Multiple Pointers:
+
+// Start with 2 pointers (i and j) at the Start of the array: (Remember: array has to be sorted.)
+//  i                    i                      i                    i                    i
+// [1,1,2,3,3,4,4,5] -> [1,1,2,3,3,4,4,5] -> [1,1,2,3,3,4,4,5] -> [1,2,2,3,3,4,4,5] -> [1,2,2,3,3,4,4,5] ....
+//    j                      j                    j                    j                      j
+
+// j will have to move to the right in a loop,(loop through the array)
+// everytime j visits a new element we compare it with i
+// -> if they are equal, ignore
+// -> if they are not equal, 1) MOVE i one position to the right
+//                           2) UPDATE/PUT i with the element in j, and continue the j loop
+
+//          i
+// [1,2,3,4,5,4,4,5]
+//                j
+
+function countUniqueValues(arr){
+    //we assume arr is sorted.
+
+    // if arr.length is 0, return 0
+    if(arr.length === 0){
+        return 0;
+    }
+
+    let i = 0;
+    let j = 1;
+    for (j; j < arr.length; j++) {       
+        if(arr[i] !== arr[j]){
+            i++;
+            arr[i] = arr[j];
+        }
+    }
+    return i + 1; // as arrays start at 0, but we want to return the count of unique values by counting from 1.
+}
+
+
