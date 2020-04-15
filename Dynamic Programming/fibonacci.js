904a15e271a1af2325ef37afaef961a059693be3
Michael Zapata <mikeyfriends@hotmail.com>
2020-04-15 11:34:03 +0100

    added tabulation

--- Dynamic Programming/fibonacci.js
+++ Dynamic Programming/fibonacci.js
@@ -16,9 +16,12 @@ function fib_memo(n, memo = [undefined, 1 ,1]){
 // Or
 // TABULATION: Start at the base and work it up to the expensive operations \
 //                  While storing previous calculated values
-function fib_tabu(n, memo = [undefined, 1 ,1]){
-    if(memo[n] !== undefined) return memo[n];
-    const result = fib_memo(n - 1, memo) + fib_memo(n - 2, memo);
-    memo[n] = result;
-    return result;
+function fib_tabu(n){
+    if(nth <= 2) return 1;
+    const fib_numbers = [undefined, 1, 1];
+    //start at index 3 since fib_numbers is filled till index 2
+    for (let i = 3; i <= n; i++) {
+        fib_numbers[i] = fib_numbers[i - 1] + fib_numbers[i - 2];
+    }
+    return fib_numbers[n];
 }
