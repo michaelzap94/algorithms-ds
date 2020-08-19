926edb9cb8d04fbcec8211895c4d293aa3638a8f
Michael Zapata <mikeyfriends@hotmail.com>
2020-08-19 19:55:27 +0100

    chunks

--- Stephen/chunk/index.js
+++ Stephen/chunk/index.js
@@ -8,6 +8,63 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+//RECURSION works but in different order
+// function chunk(array, size) {
+//     if(array.length <=0) return [];
+//     let removedElements = array.splice(0, size);
+//     let newArr = chunk(array, size);
+//     newArr.push(removedElements);
+//     return newArr;
+// }
+
+function chunk_concat_recursion(array, size) {
+    if(array.length <=0) return [];
+    return [array.splice(0, size)].concat(chunk(array, size));
+}
+
+function chunk_helper(array, size){
+    let newArr = [];
+    function helper(arr, size){
+        if(arr.length <=0) return;
+        newArr.push(arr.splice(0, size));
+        helper(arr, size);
+    }
+    helper(array, size);
+    return newArr;
+}
+
+function chunk_long(array, size){
+    let newArr = [];
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        //first get the last Array(bucket) inside the main array
+        let lastBucket = newArr[newArr.length - 1];//initially it will be undefined
+        //if undefined or if full, create a new bucket and add the current element
+        if(typeof lastBucket === 'undefined' || lastBucket.length >= size) {
+            newArr.push([element]);
+        } else {//if not full, keep adding elements to it
+            lastBucket.push(element);
+        }
+    }
+    return newArr;
+}
+
+function chunk_slice(array, size) {
+    let newArr = [];
+    let index = 0;
+    while(index < array.length ) {
+        newArr.push(array.slice(index, index+size));
+        index += size;
+    }
+    return newArr;
+}
+
+function chunk(array, size){
+    let newArr = [];
+    while(array.length > 0 ) {
+        newArr.push(array.splice(0, size));
+    }
+    return newArr;
+}
 
 module.exports = chunk;
