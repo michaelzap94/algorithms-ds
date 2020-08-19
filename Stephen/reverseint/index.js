ea256a46940bcb5f52f5f717b008b86f09a7a7d4
Michael Zapata <mikeyfriends@hotmail.com>
2020-08-19 15:53:03 +0100

    reverseInt

--- Stephen/reverseint/index.js
+++ Stephen/reverseint/index.js
@@ -8,6 +8,13 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let sign = Math.sign(n);//1 or -1
+    n = sign * n; //remove sign
+    let str = n.toString();//convert to string
+    let reversedStr = str.split('').reduce((prev,curr) => curr + prev, "");//reverse to str
+
+    return parseInt(reversedStr) * sign;//return number with original number
+}
 
 module.exports = reverseInt;
