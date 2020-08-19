3bf047f073318bb20512183d5ea7df4129d5bf3b
Michael Zapata <mikeyfriends@hotmail.com>
2020-08-19 18:30:24 +0100

    maxchar

--- Stephen/maxchar/index.js
+++ Stephen/maxchar/index.js
@@ -5,6 +5,17 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const dict = {};
+    const max = {letter:null,value:0};
+    for (let e of str) {
+        dict[e] = dict[e] ? dict[e] + 1 : 1;
+        if(dict[e] > max.value) {
+            max.letter = e;
+            max.value = dict[e];
+        }
+    }
+    return max.letter;
+}
 
 module.exports = maxChar;
