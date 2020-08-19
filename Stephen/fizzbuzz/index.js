3e7197f4d2c4bd4892982f5d31dbe2d2a20508bb
Michael Zapata <mikeyfriends@hotmail.com>
2020-08-19 18:36:51 +0100

    fizzbuzz

--- Stephen/fizzbuzz/index.js
+++ Stephen/fizzbuzz/index.js
@@ -12,6 +12,13 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    for (let i = 0; i < n; i++) {
+        if(n%3===0 && n%5===0) console.log('fizzbuzz');
+        else if (n%3===0) console.log('fizz');
+        else if (n%5===0) console.log('buzz');
+        else console.log(i);  
+    }
+}
 
 module.exports = fizzBuzz;
