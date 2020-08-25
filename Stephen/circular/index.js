906017834901451762a77f421e61651f83ad78cf
Michael Zapata <mikeyfriends@hotmail.com>
2020-08-25 13:29:02 +0100

    linkedlist circular

--- Stephen/circular/index.js
+++ Stephen/circular/index.js
@@ -12,6 +12,28 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+//INPUTS: 1 LinkedList of nodes
+//OUTPUTS: boolean
+//Output derived from input: YES
+//Requirements:return true if the list is circular, false if it is not.
+//Assumptions: not null arguments or undefined
+//Constraints: *Do not* use a counter variable, *do not* retrieve the size of the list, and only iterate through the list one time.
+
+
+function circular(list) {
+    let slow = list.head;
+    let fast = list.head;
+
+    //if this is not true at some point, we'll exit as it's not circular
+    while(fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        //compare the node itself and not the data
+        if(slow === fast){
+            return true;
+        }
+    }
+    return false;
+}
 
 module.exports = circular;
