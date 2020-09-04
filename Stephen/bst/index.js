79d1509e9be74358b7be9a66b2f7f3a8afb31d3c
Michael Zapata <mikeyfriends@hotmail.com>
2020-09-04 11:47:44 +0100

    bst using one class only

--- Stephen/bst/index.js
+++ Stephen/bst/index.js
@@ -11,6 +11,44 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+    constructor(data){
+        this.data = data;
+        this.left = null;
+        this.rigth = null;
+    }
+
+    // insert(data) {
+    //     // INSERT WHEN WE REACH TO A LEAF
+    //     let current = this;//this will be the root -> new BST(value)
+    //     while(true){
+    //         if(data < current.data){
+    //             //check if node on the left is a leaf -> null
+    //             if(current.left == null){
+    //                 //if so, insert new BST(data) node here:
+    //                 current.left = new Node(data);
+    //                 break;
+    //             } else {
+    //                 //else just move to the next node;
+    //                 current = current.left;
+    //             }
+    //         } else if(data > current.data){
+    //             //check if node on the right is a leaf -> null
+    //             if(current.right == null){
+    //                 //if so, insert new BST(data) node here:
+    //                 current.right = new Node(data);
+    //                 break;
+    //             } else {
+    //                 //else just move to the next node;
+    //                 current = current.right;
+    //             }
+    //         } else {
+    //             break;//if data already exists we just break
+    //         }
+    //     }
+        
+    //     return this;
+    //   }
+}
 
 module.exports = Node;
