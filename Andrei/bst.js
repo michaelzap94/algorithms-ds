b9558fd18be2e0e89a3335120f1815d988af601a
Michael Zapata <mikeyfriends@hotmail.com>
2020-09-08 18:24:18 +0100

    started bst implementation

---
+++ Andrei/bst.js
@@ -0,0 +1,18 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BST {
+    constructor(){
+        this.root = null;
+    }
+
+    insert(value){
+        let currentNode = this.root; // or this if BST is also the Node
+        
+    }
+}
