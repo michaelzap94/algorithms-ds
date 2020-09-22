ed554c3739dbe159fb75fba4e66de4a6cc0bd91e
Michael Zapata <mikeyfriends@hotmail.com>
2020-09-22 18:28:12 +0100

    colt's fibonacci

--- Colt/Dynamic Programming/fibonacci.js
+++ Colt/Dynamic Programming/fibonacci.js
@@ -7,12 +7,16 @@ function fib(nth) {
 // 2 solutions:
 // MEMOIZATION: Remember what expensive operations we have done. O(N) \
 //              problem: Stackoverflow can happen.\
-function fib_memo(n, memo = [undefined, 1 ,1]){
+function fib_memo(n, memo = [1, 1 ,1]){
+    //0 or 1 or 2 will not be undefined here, so return
     if(memo[n] !== undefined) return memo[n];
     const result = fib_memo(n - 1, memo) + fib_memo(n - 2, memo);
     memo[n] = result;
     return result;
 }
+console.log(fib_memo(0));
+console.log(fib_memo(1));
+console.log(fib_memo(100));
 // Or
 // TABULATION: Start at the base and work it up to the expensive operations \
 //                  While storing previous calculated values
